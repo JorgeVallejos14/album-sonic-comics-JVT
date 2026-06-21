dcf82a0c5cd676871a2def5f5b9818d6cb1c61c7
JorgeVallejos14 <jorge.vallejos14@inacapmail.cl>
2026-06-20 23:43:50 -0400

    feat: proyecto base con datos del album con ia.txt

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { stickers } from './data/stickers'
+
+function App() {
+	console.log(stickers)
+
+	return null
+}
+
+export default App
---
+++ src/data/stickers.js
@@ -0,0 +1,7 @@
+export const stickers = [
+     { id: 1, code: "SEGA00", name: "Logo SEGA", section: "Especiales", group: "Especiales", type: "especial", rarity: "foil" },
+     { id: 2, code: "SON1", name: "Sonic", section: "Héroes", group: "Team Sonic", type: "jugador", rarity: "base" },
+     { id: 3, code: "TAI2", name: "Tails", section: "Héroes", group: "Team Sonic", type: "jugador", rarity: "base" },
+     { id: 4, code: "KNU3", name: "Knuckles", section: "Héroes", group: "Team Sonic", type: "jugador", rarity: "base" },
+     { id: 5, code: "SHD4", name: "Shadow", section: "Antiheroes", group: "Team Dark", type: "jugador", rarity: "foil" }
+];
